0a2751fe0fc1ee3852efa2c37b6f9ac8e6752188
afrze <afroze@outlook.in>
2024-09-17 18:56:15 +0530

    feat: :sparkles: Todo: App partial

---
+++ 1-todo-list/src/lib/Counter.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let initialCount = 0;
+	export let maxCount: any;
+
+	console.log($$props, $$restProps);
+
+	let count = initialCount;
+
+	function increment() {
+		if (count === maxCount) return;
+		count += 1;
+	}
+</script>
+
+<button on:click={increment}>Clicks {count}</button>
+
+<style>
+	button {
+		padding: 15px 20px;
+	}
+</style>
---
+++ 1-todo-list/src/lib/TodoList.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Button from "./Button.svelte";
+
+	export let todos: any = [];
+</script>
+
+<ul>
+	{#each todos as { id, title }, index (id)}
+		{@const number = index + 1}
+		<li>{number}- {title}</li>
+	{/each}
+</ul>
+
+<form>
+	<input />
+	<Button>Add Todo</Button>
+</form>
