357caa20c9ac82e9b3c644890f9439d3de9bbfec
afrze <afroze@outlook.in>
2024-09-18 18:05:31 +0530

    feat: :sparkles: Todo: Readonly Props

--- 1-todo-list/src/App.svelte
+++ 1-todo-list/src/App.svelte
@@ -1,27 +1,69 @@
-<script>
-	import TodoList from './lib/TodoList.svelte';
-	import { v4 as uuid } from 'uuid';
+<script lang="ts">
+  import TodoList from "./lib/TodoList.svelte";
+  import { v4 as uuid } from "uuid";
 
-	const todos = [
-		{
-			id: uuid(),
-			title: 'Todo 1',
-			completed: true
-		},
-		{
-			id: uuid(),
-			title: 'Todo 2',
-			completed: false
-		},
-		{
-			id: uuid(),
-			title: 'Todo 3',
-			completed: true
-		}
-	];
+  let todoList: any;
+
+  let todos = [
+    {
+      id: uuid(),
+      title: "Todo 1",
+      completed: true,
+    },
+    {
+      id: uuid(),
+      title: "Todo 2",
+      completed: false,
+    },
+    {
+      id: uuid(),
+      title: "Todo 3",
+      completed: true,
+    },
+  ];
+
+  function handleAddTodo(event: any) {
+    event.preventDefault();
+    setTimeout(() => {
+      todos = [
+        ...todos,
+        {
+          id: uuid(),
+          title: event.detail.title,
+          completed: false,
+        },
+      ];
+
+      todoList.clearInput();
+    }, 1000);
+  }
+
+  function handleRemoveTodo(event: any) {
+    todos = todos.filter((t) => t.id !== event.detail.id);
+  }
+
+  function handleToggleTodo(event: any) {
+    todos = todos.map((t) => {
+      if (t.id === event.detail.id) {
+        return { ...t, completed: event.detail.value };
+      }
+      return { ...t };
+    });
+  }
 </script>
 
-<TodoList {todos} />
+<!-- {todoList && todoList.readOnly} -->
+<TodoList
+  {todos}
+  bind:this={todoList}
+  on:addtodo={handleAddTodo}
+  on:removetodo={handleRemoveTodo}
+  on:toggletodo={handleToggleTodo}
+/>
+
+<button on:click={() => {
+	todoList.focusInput()
+}}>focus input</button>
 
 <style>
 </style>
--- 1-todo-list/src/lib/TodoList.svelte
+++ 1-todo-list/src/lib/TodoList.svelte
@@ -1,17 +1,69 @@
+<svelte:options immutable={true} />
+
 <script lang="ts">
   import Button from "./Button.svelte";
+  import { createEventDispatcher } from "svelte";
 
-	export let todos: any = [];
+  export let todos: any = [];
+	
+  const dispatch = createEventDispatcher();
+	
+  function handleAddTodo() {
+		const isNotCancelled = dispatch(
+			"addtodo",
+      {
+				title: inputText,
+      },
+      { cancelable: true }
+    );
+    if (isNotCancelled) {
+			inputText = "";
+    }
+  }
+	
+  function handleRemoveTodo(id: any) {
+		dispatch("removetodo", {
+			id,
+    });
+  }
+	
+  function handleToggleTodo(id: any, value: any) {
+		dispatch("toggletodo", { id, value });
+  }
+	
+  export function clearInput() {
+		inputText = "";
+  }
+	
+	export function focusInput() {
+		input.focus();
+	}
+	// export const readOnly = 'test'
+	let inputText = "";
+	let input: any; 
 </script>
 
-<ul>
-	{#each todos as { id, title }, index (id)}
-		{@const number = index + 1}
-		<li>{number}- {title}</li>
-	{/each}
-</ul>
-
-<form>
-	<input />
-	<Button>Add Todo</Button>
-</form>
+<div class="todo-list-wrapper">
+  <ul>
+    {#each todos as { id, title, completed } (id)}
+      <li>
+        <label>
+          <input
+            type="checkbox"
+            checked={completed}
+            on:input={(e) => {
+              e.currentTarget.checked = completed;
+              handleToggleTodo(id, !completed);
+            }}
+          />
+          {title}
+        </label>
+        <button on:click={() => handleRemoveTodo(id)}>Remove</button>
+      </li>
+    {/each}
+  </ul>
+  <form class="add-todo-form" on:submit|preventDefault={handleAddTodo}>
+    <input bind:this={input} bind:value={inputText} />
+    <Button type="submit" disabled={!inputText}>Add</Button>
+  </form>
+</div>
