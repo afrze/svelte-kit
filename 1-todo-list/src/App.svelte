fefe2082f0fa7d9675c97285fb09a30dcd30f3b9
afrze <afroze@outlook.in>
2024-09-20 03:12:58 +0530

    feat: :sparkles: Completed basic functionality with fake API - Loops - Input - Value Binding - Updating Arrays - Bind component props - Custom events - Working with immutable data - Lifecycle functions - Binding dimensions - Tick lifecycle - Await block

--- 1-todo-list/src/App.svelte
+++ 1-todo-list/src/App.svelte
@@ -1,69 +1,144 @@
 <script lang="ts">
   import TodoList from "./lib/TodoList.svelte";
   import { v4 as uuid } from "uuid";
+  import { tick, onMount } from "svelte";
+  // import { identity } from 'svelte/internal';
 
   let todoList: any;
+  let showList = true;
 
-  let todos = [
-    {
-      id: uuid(),
-      title: "Todo 1",
-      completed: true,
-    },
-    {
-      id: uuid(),
-      title: "Todo 2",
-      completed: false,
-    },
-    {
-      id: uuid(),
-      title: "Todo 3",
-      completed: true,
-    },
-  ];
+  let todos: any = null;
+  let error: any = null;
+  let isLoading = false;
+  let isAdding = false;
+  let disabledItems: any[] = [];
 
-  function handleAddTodo(event: any) {
-    event.preventDefault();
-    setTimeout(() => {
-      todos = [
-        ...todos,
-        {
-          id: uuid(),
-          title: event.detail.title,
-          completed: false,
-        },
-      ];
+  onMount(() => {
+    loadTodos();
+  });
 
-      todoList.clearInput();
-    }, 1000);
-  }
-
-  function handleRemoveTodo(event: any) {
-    todos = todos.filter((t) => t.id !== event.detail.id);
-  }
-
-  function handleToggleTodo(event: any) {
-    todos = todos.map((t) => {
-      if (t.id === event.detail.id) {
-        return { ...t, completed: event.detail.value };
+  async function loadTodos() {
+    isLoading = true;
+    await fetch("https://jsonplaceholder.typicode.com/todos?_limit=10").then(
+      async (response) => {
+        if (response.ok) {
+          todos = await response.json();
+        } else {
+          error = "An error has occurred";
+        }
       }
-      return { ...t };
+    );
+    isLoading = false;
+  }
+
+  async function handleAddTodo(event: any) {
+    event.preventDefault();
+    isAdding = true;
+    await fetch("https://jsonplaceholder.typicode.com/todos", {
+      method: "POST",
+      body: JSON.stringify({
+        title: event.detail.title,
+        completed: false,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }).then(async (response) => {
+      if (response.ok) {
+        const todo = await response.json();
+        todos = [...todos, { ...todo, id: uuid() }];
+        todoList.clearInput();
+      } else {
+        alert("An error has occurred.");
+      }
     });
+    isAdding = false;
+    await tick();
+    todoList.focusInput();
+  }
+
+  async function handleRemoveTodo(event: any) {
+    const id = event.detail.id;
+    if (disabledItems.includes(id)) return;
+    disabledItems = [...disabledItems, id];
+    await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+      method: "DELETE",
+    }).then((response) => {
+      if (response.ok) {
+        todos = todos.filter((t: any) => t.id !== event.detail.id);
+      } else {
+        alert("An error has occurred.");
+      }
+    });
+    disabledItems = disabledItems.filter((itemId: any) => itemId !== id);
+  }
+
+  async function handleToggleTodo(event: any) {
+    const id = event.detail.id;
+    const value = event.detail.value;
+    if (disabledItems.includes(id)) return;
+    disabledItems = [...disabledItems, id];
+    await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+      method: "PATCH",
+      body: JSON.stringify({
+        completed: value,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }).then(async (response) => {
+      if (response.ok) {
+        const updatedTodo = await response.json();
+        todos = todos.map((todo: any) => {
+          if (todo.id === id) {
+            return updatedTodo;
+          }
+          return { ...todo };
+        });
+      } else {
+        alert("An error has occurred.");
+      }
+    });
+
+    disabledItems = disabledItems.filter((itemId: any) => itemId !== id);
   }
 </script>
 
-<!-- {todoList && todoList.readOnly} -->
-<TodoList
-  {todos}
-  bind:this={todoList}
-  on:addtodo={handleAddTodo}
-  on:removetodo={handleRemoveTodo}
-  on:toggletodo={handleToggleTodo}
-/>
-
-<button on:click={() => {
-	todoList.focusInput()
-}}>focus input</button>
+<label>
+  <input type="checkbox" bind:checked={showList} />
+  Show/Hide list
+</label>
+{#if showList}
+  <div style:max-width="400px">
+    <TodoList
+      {todos}
+      {error}
+      {isLoading}
+      {disabledItems}
+      disableAdding={isAdding}
+      bind:this={todoList}
+      on:addtodo={handleAddTodo}
+      on:removetodo={handleRemoveTodo}
+      on:toggletodo={handleToggleTodo}
+    >
+      <!-- <svelte:fragment slot="title">{index + 1}- {todo.title}</svelte:fragment> -->
+      <!-- {@const { id, completed, title } = todo} -->
+      <!-- <Todo {todo} on:remove on:toggle /> -->
+      <!-- <div>
+        <input
+          disabled={disabledItems.includes(id)}
+          on:input={(event) => {
+            event.currentTarget.checked = completed;
+            handleToggleTodo(id, !completed);
+          }}
+          type="checkbox"
+          checked={completed}
+        />
+        {title}
+      </div> -->
+    </TodoList>
+  </div>
+{/if}
 
 <style>
 </style>
--- 1-todo-list/src/vite-env.d.ts
+++ 1-todo-list/src/vite-env.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
 
-declare module 'uuid';
+declare module "uuid";
+declare module "svelte-icons/fa/FaRegTrashAlt.svelte";
